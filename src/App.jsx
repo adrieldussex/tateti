e67f94300521b2b56e35f899dbbd67f9e22fa7cd
adrieldussex <adrieldussex@gmail.com>
2023-04-20 12:17:09 -0300

    Create updateBoard

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,48 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Square } from './components/Square'
+import { TURNS } from '../constants'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [board, setBoard] = useState(Array(9).fill(null))
+
+  const [turn, setTurn] = useState(TURNS.X)
+
+  const updateBoard = (index) => {
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    const newTurn = turn == TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <main className='flex flex-col justify-center items-center gap-6'>
+      <h1 className='font-semibold'>TA-TE-TI</h1>
+      <section className='grid grid-cols-3 gap-2'>
+        {
+          board.map((_, index) => {
+            return (
+              <Square 
+              key={index}
+              index={index}
+              updateBoard={updateBoard}
+              >
+                {board[index]}
+              </Square>
+            )
+          })
+        }
+      </section>
+      <section className='flex flex-col gap-2'>
+        <h2 className='text-xl font-semibold'>Siguiente turno:</h2>
+        <div className='flex gap-2'>
+          <Square isSelected={turn == TURNS.X}>{TURNS.X}</Square>
+          <Square isSelected={turn == TURNS.O}>{TURNS.O}</Square>
+        </div>
+      </section>
+    </main>
   )
 }
 
