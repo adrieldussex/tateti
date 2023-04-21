90314c6185be4323c7b724f13a1acb29fb9b60bb
adrieldussex <adrieldussex@gmail.com>
2023-04-21 16:42:52 -0300

    Add logic in app

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,53 @@ import { useState } from 'react'
 import './App.css'
 import { Square } from './components/Square'
 import { TURNS } from '../constants'
+import { checkWinner, checkEndGame } from './logic/Board'
+import { FinishModal } from './components/FinishModal'
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
-
   const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null)
 
   const updateBoard = (index) => {
+    // no actualizar la posición si tiene algo
+    if (board[index] || winner) return
+    // actualizar tablero
     const newBoard = [...board]
     newBoard[index] = turn
     setBoard(newBoard)
-
+    // cambiar el turno
     const newTurn = turn == TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    // ver si hay un ganador
+    const newWinner = checkWinner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false)
+    }
+  }
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
   }
 
   return (
     <main className='flex flex-col justify-center items-center gap-6'>
       <h1 className='font-semibold'>TA-TE-TI</h1>
+      <button onClick={resetGame}>Reiniciar</button>
       <section className='grid grid-cols-3 gap-2'>
         {
-          board.map((_, index) => {
+          board.map((square, index) => {
             return (
               <Square 
               key={index}
               index={index}
               updateBoard={updateBoard}
               >
-                {board[index]}
+                {square}
               </Square>
             )
           })
@@ -42,6 +61,8 @@ function App() {
           <Square isSelected={turn == TURNS.O}>{TURNS.O}</Square>
         </div>
       </section>
+
+      <FinishModal resetGame={resetGame} winner={winner}/>
     </main>
   )
 }
