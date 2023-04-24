d29d7b77a8f649f5c57c7ac8440766ec54de7a27
adrieldussex <adrieldussex@gmail.com>
2023-04-24 15:24:56 -0300

    Styles for Modal

--- src/components/FinishModal.jsx
+++ src/components/FinishModal.jsx
@@ -4,16 +4,18 @@ export function FinishModal({winner, resetGame}) {
     const winnerText = winner == false ? 'Empate' : 'El ganador es:'
 
     return (
-          <section className='flex flex-col gap-2'>
-            <header>
-              <h2>{winnerText}</h2>
-            </header>
-            <main>
-              {winner && <p className='text-xl bg-red-200 px-6 py-4 rounded-full'>{winner}</p>}
-            </main>
-            <footer>
-              <button onClick={resetGame}>Empezar de nuevo</button>
-            </footer>
+          <section className='bg-slate-300 absolute w-screen h-screen top-0 left-0 grid place-items-center'>
+            <div className="bg-slate-200 w-80 p-4 rounded-lg flex flex-col justify-center items-center shadow-lg gap-6">
+              <header>
+                <h2>{winnerText}</h2>
+              </header>
+              <main>
+                {winner && <p className='text-4xl'>{winner}</p>}
+              </main>
+              <footer>
+                <button onClick={resetGame}>Empezar de nuevo</button>
+              </footer>
+            </div>
           </section>
   )
 }
